364ca3a33c7dabad7c3e18d2b53c058b9c2524eb
Ahmed Hafez <61163635+AhmedHafez7-Eng@users.noreply.github.com>
2020-12-02 21:56:30 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,68 +1,41 @@
-<div class="container-fluid">
+<!-- Start NavBar -->
+    <header>
+      <a href="bu.php" class="logo">BU Learn</a>
+      <div class="toggle" onclick="togglemenu();"></div>
+      <ul class="menu">
+        <li class="activeColor"><a href="bu.php" onclick="togglemenu();">Home</a></li>
+        <li><a href="bu.php #about" onclick="togglemenu();">About</a></li>
+        <li><a href="bu.php #sponsers" onclick="togglemenu();">Sponsers</a></li>
+        <li><a href="bu.php #requestDemo" onclick="togglemenu();">Request a Demo</a></li>
 
-    <nav class="navbar fixed-top navbar-expand-lg navbar-dark">
+        <?php if(isset($_SESSION['username'])){
+                echo '<li>
+                        <div class="dropdown">
+                          <button class="dropbtn">'.$_SESSION['username'].'
+                            <i class="fa fa-caret-down"></i>
+                          </button>
+                          <div class="dropdown-content">
+                            <a href="logout.php">Logout</a>
+                          </div>
+                        </div>
+                      </li>';
+              }else{
 
+                echo '<li class="registerlinks"><a href="login.php" onclick="togglemenu();">Log In</a></li>
+                      <li class="registerlinks">
+                            <div class="dropdown">
+                              <button class="dropbtn">Sign Up
+                                <i class="fa fa-caret-down"></i>
+                              </button>
+                              <div class="dropdown-content">
+                                <a href="instrsignup.php">Instructor</a>
+                                <a href="signup.php">Student</a>
+                              </div>
+                            </div>
+                      </li>';
+              }
+        ?>
+      </ul>
+    </header>
 
-        <a class="navbar-brand" href="bu.php" style="color: orange; border: 3px solid orange;padding: 7px;border-radius: 5px;">BU_Learn</a>
-
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link" href="bu.php">Home <span class="sr-only">(current)</span></a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Tour</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Resources</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
-                </li>
-            </ul>
-            <?php if(isset($_SESSION['username'])){
-                echo '<div class="my-2 my-lg-0" id="registernav">
-			      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                         
-                        <div class="borderlink"></div>
-                          <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                  '.$_SESSION['username'].'
-                                </a>
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                  <a class="dropdown-item" href="logout.php">Logout</a>
-                                </div>
-                        </li>
-                    </ul>
-                 </div>';
-            }else{
-                echo '<div class="my-2 my-lg-0" id="registernav">
-			      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                          <li class="nav-item">
-                            <a class="nav-link" href="login.php">Log In</a>
-                          </li>
-                        <div class="borderlink"></div>
-                          <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                  Sign Up
-                                </a>
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                  <a class="dropdown-item" href="instrsignup.php">Instructor</a>
-                                  <a class="dropdown-item" href="signup.php">Student</a>
-                                </div>
-                        </li>
-                    </ul>
-                 </div>';
-            }
-            ?>
-
-        </div>
-
-    </nav>
-</div>
+  <!-- End NavBar -->
